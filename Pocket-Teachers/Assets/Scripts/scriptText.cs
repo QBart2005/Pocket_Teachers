1b4b7afadddd6116c11c041e5dc877443b011667
QBart2005 <quintenbart@ziggo.nl>
2022-01-20 20:20:05 +0100

    Added basic functions
    -Added object colliders
    -Added Encounter detection
    -Added player movement
    -Added player animations
    -Fixed Clipping issue

---
+++ Pocket-Teachers/Assets/Scripts/scriptText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scriptText : MonoBehaviour
+{
+    public int i = 1;
+    public Text txt;
+
+    void Start()
+    {
+        txt.text = "Welkom bij Pocket Teachers!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(i == 1)
+        if(Input.GetButtonDown("Jump"))
+        {
+            txt.text = "Het doel van de game is om docenten te vangen en die tegen elkaar te laten vangen.";
+            i = 1;
+        }
+    }
+}
